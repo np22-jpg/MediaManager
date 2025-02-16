e38d2e872ca45c1b84dd7edda8663a0b8a1b6ca7
maxid <maximilian.dorninger@sz-ybbs.ac.at>
2025-02-16 22:22:12 +0100

    enhance reponses and openapi docs on create_user endpoint

--- MediaManager/src/routers/users.py
+++ MediaManager/src/routers/users.py
@@ -2,6 +2,8 @@ import logging
 
 from fastapi import APIRouter
 from fastapi import Depends
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
 
 import database
 from auth.password import authenticate_user, get_password_hash
@@ -12,6 +14,9 @@ router = APIRouter(
 )
 
 
+class Message(BaseModel):
+    message: str
+
 class CreateUser(User):
     """"
     The Usermodel, but with an additional non-hashed password. attribute
@@ -21,7 +26,10 @@ class CreateUser(User):
 log = logging.getLogger(__name__)
 log.level = logging.DEBUG
 
-@router.post("/")
+@router.post("/",status_code=201, responses = {
+    409: {"model":  Message, "description": "User with provided email already exists"},
+    201:{"model": UserInternal, "description": "User  created successfully"}
+})
 async def create_user(
         user: CreateUser = Depends(CreateUser),
 ):
@@ -29,10 +37,10 @@ async def create_user(
                                  hashed_password=get_password_hash(user.password))
     if database.create_user(internal_user):
         log.info("Created new user",internal_user.model_dump())
-        return user
+        return internal_user
     else:
-        log.warning("Failed to create new user", internal_user.model_dump())
-        return {"error": "Failed to create new user"}
+        log.warning("Failed to create new user, User with this email already exists,",internal_user.model_dump())
+        return JSONResponse(status_code=409, content={"message": "User with this email already exists"})
 
 
 
