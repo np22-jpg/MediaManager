c7c982e1ed7749697e67cc74bdcc92e9a50a8bd8
maxDorninger <97409287+maxDorninger@users.noreply.github.com>
2025-06-28 17:25:40 +0200

    add feature to set base url path to metadata_relay

--- metadata_relay/main.py
+++ metadata_relay/main.py
@@ -1,9 +1,11 @@
+import os
+
 from fastapi import FastAPI
 from starlette_exporter import PrometheusMiddleware, handle_metrics
 from app.tmdb import router as tmdb_router
 from app.tvdb import router as tvdb_router
 
-app = FastAPI()
+app = FastAPI(root_path=os.getenv("BASE_PATH"))
 
 app.add_middleware(PrometheusMiddleware)
 app.add_route("/metrics", handle_metrics)
