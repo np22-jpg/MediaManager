ff0a410d2c68734ba6e7ad4125328d52f736c227
maxDorninger <97409287+maxDorninger@users.noreply.github.com>
2025-06-29 23:35:59 +0200

    fix favicon not working

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-	import '../app.css';
-	import { ModeWatcher } from 'mode-watcher';
-	import { Toaster } from '$lib/components/ui/sonner/index.js';
+    import '../app.css';
+    import {ModeWatcher} from 'mode-watcher';
+    import {Toaster} from '$lib/components/ui/sonner/index.js';
 
-	let { children } = $props();
+    let {children} = $props();
 </script>
 
-<ModeWatcher />
-<Toaster />
+<svelte:head>
+    <title>MediaManager</title>
+    <meta content="Smart PVR for movies and TV shows" name="description"/>
+    <link href="/favicon.ico" rel="icon" type="ico"/>
+</svelte:head>
+
+<ModeWatcher/>
+<Toaster/>
 {@render children()}
