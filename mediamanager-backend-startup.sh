908e51c40417457ab675c12c6e107eacca98eac3
maxDorninger <97409287+maxDorninger@users.noreply.github.com>
2025-06-30 14:21:33 +0200

    update backend startup script

--- mediamanager-backend-startup.sh
+++ mediamanager-backend-startup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 # This script is used to start the MediaManager backend service.
+
+
+# text created with https://patorjk.com/software/taag/ font: Slanted
+cat << EOF
+    __  ___         ___       __  ___                                     ____             __                  __
+   /  |/  /__  ____/ (_)___ _/  |/  /___ _____  ____ _____ ____  _____   / __ )____ ______/ /_____  ____  ____/ /
+  / /|_/ / _ \/ __  / / __ `/ /|_/ / __ `/ __ \/ __ `/ __ `/ _ \/ ___/  / __  / __ `/ ___/ //_/ _ \/ __ \/ __  /
+ / /  / /  __/ /_/ / / /_/ / /  / / /_/ / / / / /_/ / /_/ /  __/ /     / /_/ / /_/ / /__/ ,< /  __/ / / / /_/ /
+/_/  /_/\___/\__,_/_/\__,_/_/  /_/\__,_/_/ /_/\__,_/\__, /\___/_/     /_____/\__,_/\___/_/|_|\___/_/ /_/\__,_/
+                                                   /____/
+EOF
+echo "Buy me a coffee at https://buymeacoffee.com/maxdorninger"
+echo "Running DB migrations..."
 uv run alembic upgrade head
+echo "Starting MediaManager backend service..."
 uv run fastapi run /app/media_manager/main.py
