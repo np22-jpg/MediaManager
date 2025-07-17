6016e3c1554be2a3d6eb87bc93892d61e4c4de79
maxDorninger <97409287+maxDorninger@users.noreply.github.com>
2025-07-17 15:17:40 +0200

    make frontend support static adapter in SPA mode

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = process.env.BASE_URL || '';
+const base = process.env.BASE_PATH || '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({fallback: '404.html'}),
 		paths: {
 			base: base
 		}
