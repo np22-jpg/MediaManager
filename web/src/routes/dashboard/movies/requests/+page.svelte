9f2ec53d239d750d9debd7804c8ebf15ebd1c9bb
maxDorninger <97409287+maxDorninger@users.noreply.github.com>
2025-08-02 12:45:55 +0200

    add the max width of the content to other pages too

--- web/src/routes/dashboard/movies/requests/+page.svelte
+++ web/src/routes/dashboard/movies/requests/+page.svelte
@@ -40,7 +40,7 @@
 	</div>
 </header>
 
-<div class="flex w-full flex-1 flex-col items-center gap-4 p-4 pt-0">
+<div class="mx-auto flex w-full flex-1 flex-col gap-4 p-4 md:max-w-[80em]">
 	<h1 class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
 		Movie Requests
 	</h1>
--- web/src/routes/dashboard/settings/+page.svelte
+++ web/src/routes/dashboard/settings/+page.svelte
@@ -40,7 +40,7 @@
 	</div>
 </header>
 
-<div class="mx-auto flex w-full max-w-[1000px] flex-1 flex-col gap-4 p-4 pt-0">
+<div class="mx-auto flex w-full flex-1 flex-col gap-4 p-4 md:max-w-[80em]">
 	<h1 class="my-6 scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
 		Settings
 	</h1>
