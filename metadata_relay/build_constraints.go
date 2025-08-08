cbb8eb2d6ab659bf36f11b49e93d65e4fc89bb19
Nolan Poe <npgo22@gmail.com>
2025-08-08 10:47:08 -0700

    build_constraints.go is no longer needed

--- metadata_relay/build_constraints.go
+++
@@ -1,7 +0,0 @@
-//go:build !test
-// +build !test
-
-package main
-
-// This file ensures that test dependencies are not included in production builds
-// Test dependencies should only be imported in files with //go:build test
